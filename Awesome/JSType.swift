24e93d874168432249bb327ea8670c591709c2d0
Marshall Brekka <marshallsmedia@gmail.com>
2015-04-03 15:35:28 -0700

    add a class for converting CG/CF types to JSValue

---
+++ Awesome/JSType.swift
@@ -0,0 +1,27 @@
+//
+//  JSType.swift
+//  Awesome
+//
+//  Class for converting CG/CF types to a JSValue
+
+import Foundation
+import JavaScriptCore
+
+class JSType {
+    
+    class func toJSValue(value: CGSize, context: JSContext) -> JSValue {
+        return JSValue(size: value, inContext: context)
+    }
+    
+    class func toJSValue(value: CGPoint, context: JSContext) -> JSValue {
+        return JSValue(point: value, inContext: context)
+    }
+
+    class func toJSValue(value: CGRect, context: JSContext) -> JSValue {
+        return JSValue(rect: value, inContext: context)
+    }
+    
+    class func toJSValue(value: CFRange, context: JSContext) -> JSValue {
+        return JSValue(object: ["location": value.location, "length": value.length], inContext: context)
+    }
+}
